074f5afa1c2ae149290277dec284bb4864938f03
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-09-30 16:45:07 +0100

    comment out list template

--- list.js
+++ list.js
@@ -1,10 +1,10 @@
 const objListMessages = {
   //template
-  title1: {
-    author: "Author long very long",
-    title: "title1",
-    text: "Message long very long long very long",
-  },
+  // title1: {
+  //   author: "Author long very long",
+  //   title: "title1",
+  //   text: "Message long very long long very long",
+  // },
 };
 
 // module.exports = { arrListMessages, objListMessages };
